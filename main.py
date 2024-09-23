5f2ff04a1755abed072305fc91dc4b8a3509b6ed
emeraldDevUA <kokhanchuk2021ks13@student.karazin.ua>
2024-09-23 10:09:11 +0200

    l2

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
 def plot_hists(segments, size):
 
     for i, segment in enumerate(segments):
@@ -36,26 +38,6 @@ def segment_image_no_overlap(image, segment_size):
 
     return segments  # Return the list of segments
 
-def greyscale(image, w, h):
-    gray_image = np.zeros((h, w), dtype=np.uint8)
-    for i in range(h):
-        for j in range(w):
-            b, g, r = image[i, j]
-            # Calculate the grayscale value using the formula
-            gray_value = int(0.299 * r + 0.587 * g + 0.114 * b)
-            # Assign the grayscale value to the new image
-            gray_image[i, j] = gray_value
-    return gray_image
-
-def threshold_processing(image, w, h, threshold):
-    th_image = np.zeros((h, w), dtype=np.uint8)
-    for i in range(h):
-        for j in range(w):
-            if image[i, j] > threshold:
-                th_image[i, j] = 0
-            else:
-                th_image[i, j] = 255
-    return th_image
 
 def get_histogram(image):
     hist, bins = np.histogram(image.flatten(), 256, [0, 256])
@@ -63,36 +45,6 @@ def get_histogram(image):
 
 image = cv2.imread('images/I23.BMP')
 
+
 height, width, channels = image.shape
 
-gray_image = greyscale(image, width, height)
-th_image = threshold_processing(gray_image, width, height, 95)
-
-hist_original = get_histogram(gray_image)
-hist_threshold = get_histogram(th_image)
-hist_grayscale = get_histogram(gray_image)
-fig, axs = plt.subplots(2, 3, figsize=(10, 8))
-
-axs[0, 0].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-axs[0, 0].set_title('Original Image')
-axs[0, 0].axis('off')
-
-axs[0, 2].imshow(th_image, cmap='gray')
-axs[0, 2].set_title('Threshold Image')
-axs[0, 2].axis('off')
-
-axs[0, 1].imshow(gray_image, cmap='gray')
-axs[0, 1].set_title('Grayscale Image')
-axs[1, 1].plot(hist_grayscale, color='black')
-
-axs[1, 0].plot(hist_original, color='black')
-axs[1, 0].set_title('Histogram (Original)')
-
-axs[1, 2].plot(hist_threshold, color='black')
-axs[1, 2].set_title('Histogram (Threshold)')
-
-plt.tight_layout()
-plt.show()
-
-
-plot_hists(segment_image_no_overlap(gray_image, 128),128)
