44105e4721cd3d423a6e21b6b5f804a4422575b8
emeraldDevUA <kokhanchuk2021ks13@student.karazin.ua>
2024-09-27 17:04:09 +0200

    l2

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def segment_image_no_overlap(image, segment_size):
     Returns:
     - List of image segments
     """
-    height, width = image.shape  # Get the dimensions of the image
+    height, width = segment_size, segment_size  # Get the dimensions of the image
     segments = []  # Initialize an empty list to store segments
 
     # Loop through the image to create segments of size `segment_size x segment_size`
@@ -72,7 +72,7 @@ def get_probs(image, w, h):
     return [p1, p2, p3, p4, p5, p6]
 
 
-def get_entropy(probs):
+def shanon_entropy(probs):
     final_value = 0
     for p in probs:
         if p > 0:
@@ -123,41 +123,53 @@ def markov_process(img):
     return transition_matrix
 
 
-def plot_combined_3d(hist, transition_matrix):
-    fig = plt.figure(figsize=(18, 12))
+def plot_combined_3d(hist, transition_matrix, avg_matrix):
+    fig = plt.figure(figsize=(24, 12))
 
     # Pixels
     ax1 = fig.add_subplot(131, projection='3d')
     x_hist = np.arange(len(hist))
     y_hist = np.zeros_like(hist)
-    ax1.bar(x_hist, hist, zs=0, zdir='y', alpha=0.8, color='blue', width=0.5)
-    ax1.set_title('Probabilities')
-    ax1.set_xlabel('Color')
+    ax1.bar(x_hist, hist, zs=0, zdir='y', alpha=0.8, color='red', width=0.5)
+    ax1.set_title('Entropy types')
+    # ax1.set_xlabel('Entropy')
     # ax1.set_ylabel('Counts')
-    ax1.set_zlabel('Probability')
+    ax1.set_zlabel('Entropy value')
 
     # Entropy
-    ax2 = fig.add_subplot(132, projection='3d')
-    ax2.bar(['Entropy'], [entropy], zs=0, zdir='y', alpha=0.8, color='orange')
-    ax2.set_title('Hartley Entropy')
-    ax2.set_xlabel('Value')
-    ax2.set_ylabel('Counts')
-    ax2.set_zlabel('Entropy Value')
+    # ax2 = fig.add_subplot(132, projection='3d')
+    # ax2.bar(['Entropy'], [entropy], zs=0, zdir='y', alpha=0.8, color='orange')
+    # ax2.set_title('Hartley Entropy')
+    # ax2.set_xlabel('Value')
+    # ax2.set_ylabel('Counts')
+    # ax2.set_zlabel('Entropy Value')
 
     # Markov Process
-    ax3 = fig.add_subplot(133, projection='3d')
+    ax3 = fig.add_subplot(132, projection='3d')
     x = np.arange(transition_matrix.shape[0])
     y = np.arange(transition_matrix.shape[1])
     x, y = np.meshgrid(x, y)
 
     z = transition_matrix.flatten()
 
-    ax3.bar3d(x.flatten(), y.flatten(), np.zeros_like(z), 1, 1, z, shade=True)
+    ax3.bar3d(x.flatten(), y.flatten(), np.zeros_like(z), 1, 1, z, shade=True, color='yellow')
     ax3.set_title('Transition Matrix')
     ax3.set_xlabel('Current Pixel Intensity')
     ax3.set_ylabel('Next Pixel Intensity')
     ax3.set_zlabel('Probability')
 
+    ax3 = fig.add_subplot(133, projection='3d')
+    x = np.arange(avg_matrix.shape[0])
+    y = np.arange(avg_matrix.shape[1])
+    x, y = np.meshgrid(x, y)
+
+    z = avg_matrix.flatten()
+
+    ax3.bar3d(x.flatten(), y.flatten(), np.zeros_like(z), 1, 1, z, shade=True)
+    ax3.set_title('Transition Matrix')
+    ax3.set_xlabel('Current Pixel Intensity')
+    ax3.set_ylabel('Next Pixel Intensity')
+    ax3.set_zlabel('Probability')
     plt.tight_layout()
     plt.show()
 
@@ -165,9 +177,23 @@ def plot_combined_3d(hist, transition_matrix):
 image = cv2.imread('images/I23.BMP')
 
 height, width, channels = image.shape
+segments = segment_image_no_overlap(image, 64)
 
 probs = get_probs(image, width, height)
-entropy = get_entropy(probs)
+entropy = shanon_entropy(probs)
+partial_sh_entropy = 0
+partial_hly_entropy = 0
+matrix_temp = -1
+for i in segments:
+    partial_sh_entropy += shanon_entropy(get_probs(i, 64, 64))
+    partial_hly_entropy += hartley_entropy(i)
+    if matrix_temp == -1:
+        matrix_temp = markov_process(i)
+
+partial_sh_entropy = partial_sh_entropy / (len(segments))
+partial_hly_entropy = partial_hly_entropy / len(segments)
+matrix_temp = matrix_temp / len(segments)
+my_array = [entropy, hartley_entropy(image), partial_sh_entropy, partial_hly_entropy]
 hist = get_histogram(image)
 transition_matrix = markov_process(image)
-plot_combined_3d(probs, transition_matrix)
+plot_combined_3d(my_array, transition_matrix, matrix_temp)
